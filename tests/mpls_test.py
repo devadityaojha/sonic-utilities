0e2a9f09f98f6276042a0d3d70de9520ce91c850
Dev Ojha <devojha.up@gmail.com>
2021-07-20 18:22:35 +0000

    Fixed test errors wrt to show interfaces mpls commands
    Signed-off-by: Dev Ojha <devojha.up@gmail.com>

--- tests/mpls_test.py
+++ tests/mpls_test.py
@@ -23,10 +23,39 @@ PortChannel2  disable
 Vlan2         enable
 """
 
+show_interfaces_mpls_output_1="""\
+Interface     MPLS State
+------------  ------------
+Ethernet2     enable
+Ethernet4     enable
+Ethernet8     disable
+Ethernet16    disable
+Loopback0     disable
+PortChannel2  disable
+Vlan2         enable
+"""
+
 show_interfaces_mpls_specific_output="""\
 Interface    MPLS State
 -----------  ------------
-Ethernet2    enable
+Ethernet4    disable
+"""
+
+show_interfaces_mpls_output_frontend="""\
+Interface     MPLS State
+------------  ------------
+Ethernet12    disable
+Ethernet16    enable
+Ethernet28    enable
+Ethernet40    disable
+Loopback0     disable
+PortChannel2  disable
+Vlan2         enable
+"""
+
+show_interfaces_mpls_output_all="""\
+Interface    MPLS State
+-----------  ------------
 """
 
 modules_path = os.path.join(os.path.dirname(__file__), "..")
@@ -53,16 +82,25 @@ class TestMpls(object):
         assert result.exit_code == 0
         assert db.cfgdb.get_entry("INTERFACE", "Ethernet4") == {"mpls": "enable"}
 
-    def test_config_mpls_remove(self):
+    def test_show_interfaces_mpls_frontend(self):
+        jsonfile = os.path.join(mock_db_path, 'appl_f_db')
+        dbconnector.dedicated_dbs['APPL_DB'] = jsonfile
+        
         runner = CliRunner()
-        db = Db()
-        obj = {'config_db':db.cfgdb}
+        result = runner.invoke(show.cli.commands["interfaces"].commands["mpls"], [])
+        print(result.output) 
+        assert result.exit_code == 0
+        assert result.output == show_interfaces_mpls_output_frontend
 
-        result = runner.invoke(config.config.commands["interface"].commands["mpls"].commands["remove"], ["Ethernet4"], obj=obj)
-        print(result.exit_code)
+    def test_show_interfaces_mpls_frontend_all(self):
+        jsonfile = os.path.join(mock_db_path, 'appl_f_db')
+        dbconnector.dedicated_dbs['APPL_DB'] = jsonfile
+
+        runner = CliRunner()
+        result = runner.invoke(show.cli.commands["interfaces"].commands["mpls"], ["all"])
         print(result.output)
         assert result.exit_code == 0
-        assert db.cfgdb.get_entry("INTERFACE", "Ethernet4") == {"mpls": "disable"}
+        assert result.output == show_interfaces_mpls_output_all
 
     def test_show_interfaces_mpls(self):
         jsonfile = os.path.join(mock_db_path, 'appl_db')
@@ -75,16 +113,39 @@ class TestMpls(object):
         assert result.exit_code == 0
         assert result.output == show_interfaces_mpls_output
 
+    def test_show_interfaces_mpls_dynamic(self):
+        jsonfile = os.path.join(mock_db_path, 'appl1_db')
+        dbconnector.dedicated_dbs['APPL_DB'] = jsonfile
+
+        runner = CliRunner()
+        result = runner.invoke(show.cli.commands["interfaces"].commands["mpls"], [])
+        print(result.exit_code)
+        print(result.output)
+        assert result.exit_code == 0
+        assert result.output == show_interfaces_mpls_output_1 
+    
     def test_show_interfaces_mpls_specific(self):
         jsonfile = os.path.join(mock_db_path, 'appl_db')
         dbconnector.dedicated_dbs['APPL_DB'] = jsonfile
 
         runner = CliRunner()
-        result = runner.invoke(show.cli.commands["interfaces"].commands["mpls"], ["Ethernet2"])
+        result = runner.invoke(show.cli.commands["interfaces"].commands["mpls"], ["Ethernet4"])
         print(result.exit_code)
         print(result.output)
         assert result.exit_code == 0
         assert result.output == show_interfaces_mpls_specific_output
+    
+    def test_config_mpls_remove(self):
+        runner = CliRunner()
+        db = Db()
+        obj = {'config_db':db.cfgdb}
+
+        result = runner.invoke(config.config.commands["interface"].commands["mpls"].commands["remove"], ["Ethernet4"], obj=obj)
+        print(result.exit_code)
+        print(result.output)
+        assert result.exit_code == 0
+        assert db.cfgdb.get_entry("INTERFACE", "Ethernet4") == {"mpls": "disable"}
+
 
     @classmethod
     def teardown_class(cls):
